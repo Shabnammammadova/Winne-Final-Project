cfbd45f20e8e998f4a7e07bfbf055d68f34f762f
shabnam <shabnamjm@code.edu.az>
2025-01-26 23:40:23 +0400

    feat:blog detail video added

--- client/src/pages/blog/components/BlogDetail.tsx
+++ client/src/pages/blog/components/BlogDetail.tsx
@@ -3,67 +3,23 @@ import { HomeBlog } from "@/pages/home/components/Blog"
 export const BlogDetail = () => {
     return (
         <div className="bg-white font-sans pt-5 border-b border-b-gray-200">
-            <div className="container mx-auto text-center flex justify-between md:flex-col xs:items-center lg:flex-row gap-20">
-                <div className="w-[500px] flex flex-col">
-                    <p className="text-start pb-5 text-[15px] font-medium uppercase tracking-[.2em] border-b border-b-gray-200">
-                        Recent posts
-                    </p>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-3 pt-[14px] pb-[24px] border-b border-b-gray-200">
-                        <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog1_1024x1024.png?v=1653885697" alt="" className="w-[80px] h-[80px] object-cover cursor-pointer" />
-                        <div className="flex text-start flex-col cursor-pointer">
-                            <p className="text-[11px] text-gray-400 font-medium tracking-[.1em] pb-[5px]">30.May.2022</p>
-                            <span className="text-black transition-all duration-300 hover:text-red-800 font-semibold">How to make handmade wine</span>
-                        </div>
-                    </div>
-                </div>
+            <div className="container mx-auto text-center flex justify-between flex-col xs:items-center gap-20">
                 <div className="w-full max-w-[1180px]">
-                    <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog2_1024x1024.png?v=1653885473" alt="" className="pb-5 object-cover" />
+                    <img src="https://winne-store-demo.myshopify.com/cdn/shop/articles/blog2_1024x1024.png?v=1653885473" alt="" className="pb-5 object-cover w-full  max-w-[1180px]" />
                     <p className="mx-auto text-center text-[22px] font-medium pb-[10px] text-black cursor-pointer hover:text-red-800">Popular types of wine today</p>
-                    <span className="w-full text-sm text-start flex items-start font-normal text-gray-400 pt-[10px]">
+                    <span className="w-full text-sm text-start flex items-start font-normal text-gray-400 py-[20px]">
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
                     </span>
+                    <div className="relative w-full pb-[56.25%]">
+                        <iframe
+                            src="https://www.youtube.com/embed/yYQOjWlQkxg?rel=0"
+                            className="absolute top-0 left-0 w-full h-full border-0"
+                            allowFullScreen
+                            scrolling="no"
+                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+                        </iframe>
+                    </div>
+
                 </div>
             </div>
             <HomeBlog />
