e522b3df5b646f9178a21dd461e279e951f3abeb
shabnam <shabnamjm@code.edu.az>
2025-02-13 20:08:27 +0400

    feat:server side stripe added

--- client/src/pages/(dashboard)/main/index.tsx
+++ client/src/pages/(dashboard)/main/index.tsx
@@ -3,39 +3,52 @@ import { ScrollToTop } from "@/components/shared/ScrollToTop"
 const DashboardMainPage = () => {
     return (
         <div>
-            <div>
-                <main className="relative  flex flex-col justify-center bg-white overflow-hidden">
-                    <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
-                        <section className="grid md:grid-cols-3 gap-6 max-md:max-w-xs mx-auto">
-                            <div className="group [background:linear-gradient(theme(colors.slate.900),theme(colors.slate.900))_padding-box,linear-gradient(45deg,theme(colors.slate.800),theme(colors.slate.600/.8),theme(colors.slate.800))_border-box] relative before:absolute before:inset-0 before:bg-[url('./noise.png')] before:bg-[length:352px_382px] rounded-2xl border border-transparent">
-                                <div className="relative">
-                                    <div className="px-6 py-5">
-                                        <div className="font-nycd text-lg text-red-500 mb-1">Label</div>
-                                        <div className="text-lg font-bold text-slate-200 mb-1">Daily Reports</div>
-                                        <p className="text-sm text-slate-500">Building truly great products is both art and science. It's part intuition and part data.</p>
-                                    </div>
-                                    <div className="relative group-hover:-translate-y-1 transition-transform duration-500 ease-in-out">
-                                        <img className="group-hover:opacity-0 transition-opacity duration-500" src="https://cruip-tutorials.vercel.app/cards-hover-effect//card-01.png" width="350" height="240" alt="Card image 01" />
-                                        <img className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500" src="https://cruip-tutorials.vercel.app/cards-hover-effect//card-01-hover.png" width="350" height="240" alt="Card image 01 displaying on hover" aria-hidden="true" />
-                                    </div>
-                                </div>
+            <div className="bg-white rounded-md w-full text-start dark:bg-black flex justify-center items-center">
+
+                <div className="p-2 flex items-start flex-col justify-start">
+                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Our
+                        statistics</h2>
+                    <div className="grid grid-cols-1 gap-5 sm:grid-cols-4 mt-4">
+                        <div className="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                            <div className="px-4 py-5 sm:p-6">
+                                <dl>
+                                    <dt className="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Total free
+                                        servers</dt>
+                                    <dd className="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">1.6M</dd>
+                                </dl>
                             </div>
-                            <div className="group [background:linear-gradient(theme(colors.slate.900),theme(colors.slate.900))_padding-box,linear-gradient(45deg,theme(colors.slate.800),theme(colors.slate.600/.8),theme(colors.slate.800))_border-box] relative before:absolute before:inset-0 before:bg-[url('./noise.png')] before:bg-[length:352px_382px] rounded-2xl border border-transparent">
-                                <div className="relative">
-                                    <div className="px-6 py-5">
-                                        <div className="font-nycd text-lg text-red-500 mb-1">Label</div>
-                                        <div className="text-lg font-bold text-slate-200 mb-1">Powerful Analytics</div>
-                                        <p className="text-sm text-slate-500">Building truly great products is both art and science. It's part intuition and part data.</p>
-                                    </div>
-                                    <div className="relative group-hover:-translate-y-1 transition-transform duration-500 ease-in-out">
-                                        <img className="group-hover:opacity-0 transition-opacity duration-500" src="https://cruip-tutorials.vercel.app/cards-hover-effect//card-03.png" width="350" height="240" alt="Card image 03" />
-                                        <img className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500" src="https://cruip-tutorials.vercel.app/cards-hover-effect//card-03-hover.png" width="350" height="240" alt="Card image 03 displaying on hover" aria-hidden="true" />
-                                    </div>
-                                </div>
+                        </div>
+                        <div className="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                            <div className="px-4 py-5 sm:p-6">
+                                <dl>
+                                    <dt className="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Servers a
+                                        month</dt>
+                                    <dd className="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">19.2K
+                                    </dd>
+                                </dl>
                             </div>
-                        </section>
+                        </div>
+                        <div className="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                            <div className="px-4 py-5 sm:p-6">
+                                <dl>
+                                    <dt className="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Servers a
+                                        week</dt>
+                                    <dd className="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">4.9K</dd>
+                                </dl>
+                            </div>
+                        </div>
+                        <div className="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                            <div className="px-4 py-5 sm:p-6">
+                                <dl>
+                                    <dt className="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Total users
+                                    </dt>
+                                    <dd className="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">166.7K
+                                    </dd>
+                                </dl>
+                            </div>
+                        </div>
                     </div>
-                </main>
+                </div>
             </div>
             <ScrollToTop />
         </div>
---
+++ server/src/controllers/checkout.ts
@@ -0,0 +1,24 @@
+import { formatAmountForStripe } from '../utils/helpers';
+import { stripe } from '../lib/stripe';
+
+
+export async function createCheckoutSession(amount: number): Promise<{ url: string | null }> {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: { name: 'Product Name' },
+                    unit_amount: formatAmountForStripe(amount, 'usd'),
+                },
+                quantity: 1,
+            }
+        ],
+        success_url: `${process.env.BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.BASE_URL}/cancel`,
+    });
+
+    return { url: session.url };
+}
